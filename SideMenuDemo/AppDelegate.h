63b917e5ed0bea741c0ecd9822fb406d7042cd08
chinabrant <sjwu1234@gmail.com>
2013-08-13 17:29:22 +0800

    Initial Commit

---
+++ SideMenuDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SideMenuDemo
+//
+//  Created by wusj on 13-8-13.
+//  Copyright (c) 2013年 brant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
